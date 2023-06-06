e7075227782d27dac099fcb29e93945ad13bff2a
Mariangithubit <metsyb1@hotmail.com>
2023-06-06 15:14:34 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def to_upper((character):
-        if ord(chararcter >= 97 and ord(character) <= 122:
+        if ord(character >= 97 and ord(character) <= 122:
             return (ord(character) - 32)
             else:
             return ord(character)
+
 def uppercase(str):
     new = ""
     for character in str:
