178b81b092c229426a4a27194f774599036d271b
Mariangithubit <metsyb1@hotmail.com>
2023-06-06 15:00:16 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def to_upper((character):
+        if ord(chararcter >= 97 and ord(character) <= 122:
+            return (ord(character) - 32)
+            else:
+            return ord(character)
+def uppercase(str):
+    new = ""
+    for character in str:
+        new += "%c" % to_upper(character)
+        print("{:s}".formatO(new))
