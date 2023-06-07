45d8a4e85a50f2d0b951cf724517a18bee7b30aa
Mariangithubit <metsyb1@hotmail.com>
2023-06-06 18:40:50 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("FizzBuzz ", end="")
         elif (num % 3 == 0):
             print("Fizz ", end="")
-        elif (num % 5):
+        elif (num % 5 == 0):
             print("Buzz ", end="")
         else:
-            print(f"{num} ", end="") 
+            print(f"{num} ", end="")
