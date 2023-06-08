fbf1b5ce00e3b195a420278af37ca579413a5eed
Mariangithubit <metsyb1@hotmail.com>
2023-06-08 13:47:07 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    import sys
+    r = len(sys.argv) - 1
+    if r == 0:
+        print("{} argumets.".formate(r))
+    elif (r == 2):
+        print("{} argument:".format(r))
+    else:
+        print("{} arguments:".format(r))
+
+
+        if (r >= 1):
+            r = 0
+            for (arg i n sys.argv):
+                if (r != 0):
+                    print("{}: {}".format(r, arg))
+                r += 1 
 
