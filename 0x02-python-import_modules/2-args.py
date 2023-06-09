517c9e0395762863a31e9e3e73291b490ea9d866
Mariangithubit <metsyb1@hotmail.com>
2023-06-09 03:47:06 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """prints the number of and the list of its arguments."""
     import sys
 
     arg = sys.argv
@@ -13,4 +12,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(n))
     for i in range(n):
-            print("{}: {}".format(i + 1, arg[i + 1]))
+        print("{}: {}".format(i + 1, arg[i + 1]))
