dd0000a00531f36e557c64903227353f3246643d
Mariangithubit <metsyb1@hotmail.com>
2023-06-08 17:51:17 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
         print("{} arguments:".format(n))
         for i in range(1, n + 1):
             print("{}: {}".format(i, arg[i]))
+
     elif n == 0:
         print("{} argumets.".format(n))
+
     else:
         print("{} argument:".format(n))
         print("{}: {}".format(n, arg[1]))
