49998f6cd823f2e58a1c0e7b9d586efa0b47e755
Mariangithubit <metsyb1@hotmail.com>
2023-06-26 12:35:19 -0700

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,6 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    result  = 0
     for i in range(0, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -17,4 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-    return new_list
+    return (new_list)
