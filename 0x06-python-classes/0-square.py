6ec4f4aa919f8b84cb8185861bf138df6e489a37
Mariangithubit <metsyb1@hotmail.com>
2023-06-27 15:16:14 -0700

    define square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""A module for square""" 
-
-
+"""A module for first square""" 
 
 class Square:
-    """Class represent a square."""
+    """Class represent a new square."""
     pass
