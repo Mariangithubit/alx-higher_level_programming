86b2a21e94b022894a34c994349a39d4a6f98ec3
Mariangithubit <metsyb1@hotmail.com>
2023-06-27 16:16:31 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,6 +18,8 @@ class Square:
     """
 
      def __init__(self, size=0):
+         """initialize a new square"""
+
          if type(size) is not int:
              raise TypeError('size must be an integer')
          if size < 0:
