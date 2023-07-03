ddfb8ebb3e7f72ed70a682ec75681729ce6c94dd
Mariangithubit <metsyb1@hotmail.com>
2023-07-03 14:29:56 -0700

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -6,40 +6,36 @@ class Rectangle:
     """Defines a rectangle"""
     def __init__(self, width=0, height=0):
         """Instantiation"""
-
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """Retrieve it"""
         return self.__width
+    
+    @property
+    def height(self):
+       """Retrieve it"""
+       return self.__height
 
     @width.setter
     def width(self, value):
         """set width"""
-        self.__width = value
-        try:
-            assert type(self.__width) == int
-        except:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
-   @property
-    def height(self):
-        """Retrieve it"""
-        return self.__height
     @height.setter
     def height(self, value):
         """set height"""
-        self.__height = value
-        try:
-            assert type(self.__height) == int
-        except:
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Returns the rectangle area"""
@@ -55,9 +51,7 @@ class Rectangle:
         """ print the rectangle with the character #"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        for i in range(self.__width):
-            for k in range(self.__height):
-                return ("\n".join(["".join(["#"])]))
+        return ("\n".join(["".join(["#" for i in range(self.__width)]) for k in range(self.__height)]))
 
     def __repr__(self):
         """representation of the rectangle"""
