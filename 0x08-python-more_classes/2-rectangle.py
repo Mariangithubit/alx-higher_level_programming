0a4d6c7232fda397a9669497b15a4da92f677927
Mariangithubit <metsyb1@hotmail.com>
2023-07-03 12:20:39 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,40 +6,36 @@ class Rectangle:
     """Defines a rectangle"""
     def __init__(self, width=0, height=0):
         """"instantiation"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """To retrieve it"""
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        """set width"""
-        self.__width = value
-        try:
-            assert type(self.__width) == int
-        except:
-            raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be >= 0")
-
     @property
     def height(self):
          """To retrieve it"""
          return self.__height
 
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     @height.setter
     def height(self, value):
          """set height"""
-         self.__height == value
-         try:
-             assert type(self.__height) == int
-         except:
+         if type(value) != int:
              raise TypeError("height must be an integer")
-         if self.__height < 0:
+         if value < 0:
              raise ValueError("height must be >= 0")
+         self.__height = value
 
     def area(self):
         """Returns the rectangle area"""
