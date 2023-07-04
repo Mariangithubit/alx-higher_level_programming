ded2607331fe85febdf133dbfeb438c5d2715674
Mariangithubit <metsyb1@hotmail.com>
2023-07-04 15:16:25 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,19 +6,16 @@ def text_indentation(text):
     """
      prints a text with 2 new lines
      after each of these characters: ., ? and :
-     """
-     if type(text) is not str:
-          raise TypeError("text must be a string")
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
 
-      s = text[:]
-      for i in ".?:":
-          list_text = s.split(i)
-          s = ""
-          for j in list_text:
-              j = j.strip(" ")
-            if s is "":
-                s = i + j
-            else:
-                s + "\n\n" + i + j
+    s = text[:]
+    for i in ".?:":
+        list_text = s.split(i)
+        s = ""
+        for j in list_text:
+            j = j.strip(" ")
+            s = i + j if s == "" else s + "\n\n" + i + j
 
     print(s[:-3], end="")
