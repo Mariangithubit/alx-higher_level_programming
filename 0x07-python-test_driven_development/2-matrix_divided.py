2c7779f0e091529375a625ca2a9b2d36233f31ec
Mariangithubit <metsyb1@hotmail.com>
2023-07-04 14:29:51 -0700

    0.Integers addition

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Divide a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """divides all elements of a matrix"""
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+         raise ZeroDivisionError("division by zero")
+     if not matrix or not isinstance(matrix, list):
+          raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    len_e = 0
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError("Each row of the matrix must have the same size")
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        len_e = len(elems)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Text indentation"""
+
+
+def text_indentation(text):
+    """
+     prints a text with 2 new lines
+     after each of these characters: ., ? and :
+     """
+     if type(text) is not str:
+          raise TypeError("text must be a string")
+
+      s = text[:]
+      for i in ".?:":
+          list_text = s.split(i)
+          s = ""
+          for j in list_text:
+              j = j.strip(" ")
+            if s is "":
+                s = i + j
+            else:
+                s + "\n\n" + i + j
+
+    print(s[:-3], end="")
