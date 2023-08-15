ee38a1cc3f73a90b971d63aaa6d39ac8c035b17f
Mariangithubit <metsyb1@hotmail.com>
2023-08-15 14:14:24 -0700

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,3 @@
 -- Creates a table called first_table in the current database in my MySQL server.
 -- The database name will be passed as an argument of the mysql command.
-CREAT TABLE IF NOT EXISTS first_table (id INT,
-	name VARCHAR(256)
-	);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
