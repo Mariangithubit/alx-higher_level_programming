340ee04f415a552e0f026eef6e08d6e8e7d2ddc7
Mariangithubit <metsyb1@hotmail.com>
2023-08-16 16:41:35 -0700

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,4 +4,4 @@ SELECT `title`, SUM(`rate`) AS `rating`
        	INNER JOIN `tv_show_ratings` AS r
        	ON t.`id` = r.`show_id`
 	GROUP BY `title`
-	ORDER BY `rating` DESC
+	ORDER BY `rating` DESC;
