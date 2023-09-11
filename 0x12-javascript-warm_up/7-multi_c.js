66c9037ac593d267368031ce5e49aabcea0178fa
Mariangithubit <metsyb1@hotmail.com>
2023-09-11 15:59:54 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 const myargs = process.argv.slice(2);
 let i = 0;
 if (isNaN(myargs[0])) {
-	console.log('Missing number of occurrences');
-}
-else {
-	while (i < parseInt(myargs[0])) {
-		console.log('C is fun');
-		i++;
-	}
+  console.log('Missing number of occurrences');
+} else {
+  while (i < parseInt(myargs[0])) {
+    console.log('C is fun');
+    i++;
+  }
 }
