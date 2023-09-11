39ee531abde83aeb162a45b624baefa2c61223ca
Mariangithubit <metsyb1@hotmail.com>
2023-09-11 14:51:06 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myargs = process.argv.slice(2);
+if (!myargs[0] || !myargs[2]) {
+	console.log(0);
+} else {
+	const args = myargs.map(Number);
+	const firstMax = Math.max.apply(null, args);
+	args.splice(args.indexOf(firstMax), 1);
+	console.log(Math.max.apply(null, args));
+}
