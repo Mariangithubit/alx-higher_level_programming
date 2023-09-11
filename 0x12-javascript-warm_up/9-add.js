d2fe26c545c1a9e80d8de7effb1cbdad033fca3e
Mariangithubit <metsyb1@hotmail.com>
2023-09-11 14:33:28 -0700

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myargs = process.argv.slice(2);
+let result = 0;
+function add(a, b) {
+	result = a + b;
+	return result;
+}
+add(parseInt(myargs[0]), parseInt(myargs[1]));
+console.log(result);
