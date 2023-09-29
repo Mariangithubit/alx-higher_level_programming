ecd907dd9206dbc80125fbde7544109cd3a036f2
Mariangithubit <metsyb1@hotmail.com>
2023-09-29 15:07:05 -0700

    What's my status?

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format('html.text.__class__'))
+    print("\t- type: {}".format(html.text.__class__))
     print("\t- content: {}".format(html.text))
