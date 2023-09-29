82d369d91358f18d3f0cf43bc03a6bd609049152
Mariangithubit <metsyb1@hotmail.com>
2023-09-29 15:15:24 -0700

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ from urllib import request, error
 
 
 if __name__ == "__main__":
-try:
-    with request.urlopen(sys.argv[1]) as sour:
-        print(sour.read().decode("utf-8"))
-except error.HTTPError as err:
-    print('Error code:', err.code)
+    try:
+        with request.urlopen(sys.argv[1]) as sour:
+            print(sour.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print('Error code:', err.code)
